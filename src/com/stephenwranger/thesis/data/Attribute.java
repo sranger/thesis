f1a46c85b572952ccfb54604f7da206dd3d0228e
rangers <rangers@3b02bc8f-ef1d-4a7e-bdb2-4513a7f1e1fa>
2016-06-13 13:51:31 +0000

    rearranged contents; moved proposal files to docs directory and started an eclipse project with the initial AFRL point data parser that curently only outputs a flat file version of the input dataset

---
+++ src/com/stephenwranger/thesis/data/Attribute.java
@@ -0,0 +1,59 @@
+package com.stephenwranger.thesis.data;
+
+import java.nio.ByteBuffer;
+
+public class Attribute {
+   public static final String HEADER = "index,name,offset,size,type,typeBytes,min,max,mean,variance";
+   
+   public final int index;
+   public final String name;
+   public final int offset;
+   public final int size;
+   public final AttributeType type;
+   public final int typeBytes;
+   public final double min;
+   public final double max;
+   public final double mean;
+   public final double variance;
+   
+   public Attribute(final String csvLine) {
+      final String[] split = csvLine.split(",");
+      
+      this.index = Integer.parseInt(split[0]);
+      this.name = split[1];
+      this.offset = Integer.parseInt(split[2]);
+      this.size = Integer.parseInt(split[3]);
+      this.type = AttributeType.valueOf(split[4]);
+      this.typeBytes = Integer.parseInt(split[5]);
+      this.min = Double.parseDouble(split[6]);
+      this.max = Double.parseDouble(split[7]);
+      this.mean = Double.parseDouble(split[8]);
+      this.variance = Double.parseDouble(split[9]);
+   }
+   
+   public Number getValue(final ByteBuffer buffer) {
+      return this.type.get(buffer, this.offset);
+   }
+   
+   public byte[] getBytes(final ByteBuffer buffer) {
+      return this.type.getBytes(buffer, this.offset, this.size);
+   }
+   
+   @Override
+   public String toString() {
+      final StringBuilder sb = new StringBuilder();
+
+      sb.append(index).append(",");
+      sb.append(name).append(",");
+      sb.append(offset).append(",");
+      sb.append(size).append(",");
+      sb.append(type).append(",");
+      sb.append(typeBytes).append(",");
+      sb.append(min).append(",");
+      sb.append(max).append(",");
+      sb.append(mean).append(",");
+      sb.append(variance);
+      
+      return sb.toString();
+   }
+}
