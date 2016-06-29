8735d53b84a739ddff5bed81b8244f4bdef01c80
rangers <rangers@3b02bc8f-ef1d-4a7e-bdb2-4513a7f1e1fa>
2016-06-29 18:29:34 +0000

    updated Icosatet to store points in specified cells based on barycentric coordinates and re-worked how TreeCell inserts points in order to reuse common code better between Octet and Icosatet

--- src/java/com/stephenwranger/thesis/octree/Octet.java
+++ src/java/com/stephenwranger/thesis/octree/Octet.java
@@ -1,7 +1,6 @@
 package com.stephenwranger.thesis.octree;
 
 import com.stephenwranger.graphics.bounds.BoundingBox;
-import com.stephenwranger.graphics.bounds.BoundingVolume;
 import com.stephenwranger.graphics.math.Tuple3d;
 import com.stephenwranger.thesis.data.Point;
 import com.stephenwranger.thesis.data.TreeCell;
@@ -9,74 +8,57 @@ import com.stephenwranger.thesis.data.TreeStructure;
 
 public class Octet extends TreeCell {
    private static final int MAX_CHILDREN = 8;
+   private final Tuple3d pending = new Tuple3d();
    private final Tuple3d current = new Tuple3d();
-   private final Tuple3d previous = new Tuple3d();
    
    Octet(final TreeStructure tree, final String path) {
       super(tree, path);
    }
-   
+
    @Override
-   protected TreeCell addPoint(final TreeStructure tree, final Point point) {
+   protected int getIndex(final TreeStructure tree, final Point point) {
       final BoundingBox bounds = (BoundingBox) this.getBoundingVolume();
       final int[] cellSplit = this.getCellSplit();
-      point.getXYZ(tree, this.current);
-      
-      if(!bounds.contains(this.current)) {
-         System.err.println("\npoint put in wrong octet: " + this.current);
-         System.out.println("current octet: " + this.path);
-         for(int i = this.path.length(); i > 0; i--) {
-            final String parent = this.path.substring(0, i);
-            final BoundingVolume parentBounds = tree.getBoundingVolume(parent);
-            System.out.println("\tin '" + parent + "'? " + parentBounds.contains(this.current) + ", " + parentBounds);
-         }
-         final BoundingVolume parentBounds = tree.getBoundingVolume("");
-         System.out.println("\tin ''? " + parentBounds.contains(this.current) + ", " + parentBounds);
-      }
-      
+      point.getXYZ(tree, this.pending);
+
       final Tuple3d min = bounds.getMin();
       final Tuple3d dimensions = bounds.getDimensions();
       final double xStep = dimensions.x / cellSplit[0];
       final double yStep = dimensions.y / cellSplit[1];
       final double zStep = dimensions.z / cellSplit[2];
       
-      final int xIndex = (int)Math.floor((this.current.x - min.x) / xStep);
-      final int yIndex = (int)Math.floor((this.current.y - min.y) / yStep);
-      final int zIndex = (int)Math.floor((this.current.z - min.z) / zStep);
+      final int xIndex = (int)Math.floor((this.pending.x - min.x) / xStep);
+      final int yIndex = (int)Math.floor((this.pending.y - min.y) / yStep);
+      final int zIndex = (int)Math.floor((this.pending.z - min.z) / zStep);
       final int index = xIndex + yIndex * cellSplit[0] + zIndex * cellSplit[0] * cellSplit[1];
       
-      final Point p = this.getPoint(index);
-      TreeCell insertedInto = this;
+      return index;
+   }
+   
+   @Override
+   protected boolean swapPointCheck(final TreeStructure tree, final Point current, final Point pending) {
+      final BoundingBox bounds = (BoundingBox) this.getBoundingVolume();
+      final int[] cellSplit = this.getCellSplit();
+
+      final Tuple3d min = bounds.getMin();
+      final Tuple3d dimensions = bounds.getDimensions();
+      final double xStep = dimensions.x / cellSplit[0];
+      final double yStep = dimensions.y / cellSplit[1];
+      final double zStep = dimensions.z / cellSplit[2];
       
-      if(p == null) {
-         this.addPoint(index, point);
-      } else {
-         final Tuple3d center = new Tuple3d(xIndex * xStep + xStep * 0.5, yIndex * yStep + yStep * 0.5, zIndex * zStep + zStep * 0.5);
-         final Tuple3d currentLocal = new Tuple3d(this.current);
-         currentLocal.subtract(min);
-         final Tuple3d storedLocal = p.getXYZ(tree, previous);
-         storedLocal.subtract(min);
-         
-         final TreeCell child = getChildCell(tree, this.current);
-         
-         if(child == null) {
-            System.out.println("error point: " + point);
-            throw new RuntimeException("Point given (" + this.current + ") does not belong in this octet (" + bounds + ")");
-         }
-         
-         insertedInto = child;
-         
-         // if new point is closer to cell center than old point, swap them
-         // then pass point to child
-         if(center.distance(currentLocal) < center.distance(storedLocal)) {
-            final Point oldPoint = this.swapPoint(index, point);
-            child.addPoint(oldPoint);
-         } else {
-            child.addPoint(point);
-         }
-      }
+      final Tuple3d pendingLocal = pending.getXYZ(tree, this.pending);
+      pendingLocal.subtract(min);
+      final Tuple3d currentLocal = current.getXYZ(tree, this.current);
+      currentLocal.subtract(min);
       
-      return insertedInto;
+      final int xIndex = (int)Math.floor((pendingLocal.x) / xStep);
+      final int yIndex = (int)Math.floor((pendingLocal.y) / yStep);
+      final int zIndex = (int)Math.floor((pendingLocal.z) / zStep);
+      
+      final Tuple3d center = new Tuple3d(xIndex * xStep + xStep * 0.5, yIndex * yStep + yStep * 0.5, zIndex * zStep + zStep * 0.5);
+      
+      // if new point is closer to cell center than old point, swap them then pass point to child
+      return center.distance(pendingLocal) < center.distance(currentLocal);
    }
 
    @Override
