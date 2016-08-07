76b0e459649f46f77616842148b059ca59a498e0
Stephen <stephen.w.ranger@gmail.com>
2016-08-07 19:51:38 +0000

    added some form of marching cubes to ContextAwarePointSelection

--- src/java/com/stephenwranger/thesis/data/DataAttributes.java
+++ src/java/com/stephenwranger/thesis/data/DataAttributes.java
@@ -9,115 +9,122 @@ import java.util.List;
 import com.stephenwranger.graphics.math.Tuple3d;
 
 public class DataAttributes implements Iterable<Attribute> {
-   private static final int BYTES_PER_POINT = 32;
-   private static final String X_ATTRIBUTE_NAME = "X";
-   private static final String Y_ATTRIBUTE_NAME = "Y";
-   private static final String Z_ATTRIBUTE_NAME = "Z";
-   private static final String R_ATTRIBUTE_NAME = "Red";
-   private static final String G_ATTRIBUTE_NAME = "Green";
-   private static final String B_ATTRIBUTE_NAME = "Blue";
-   private static final String A_ATTRIBUTE_NAME = "Altitude";
-   private static final String I_ATTRIBUTE_NAME = "Intensity";
-   
-   private static final String[] USED_ATTRIBUTE_NAMES = new String[] { X_ATTRIBUTE_NAME, Y_ATTRIBUTE_NAME, 
-         Z_ATTRIBUTE_NAME, R_ATTRIBUTE_NAME, G_ATTRIBUTE_NAME, B_ATTRIBUTE_NAME, A_ATTRIBUTE_NAME, I_ATTRIBUTE_NAME };
-   
-   private static final boolean[] normalize = new boolean[] { false, false, false, true, true, true, false, true };
-   
-   private final List<Attribute> attributes = new ArrayList<>();
-   private final Comparator<Attribute> indexComparator = new Comparator<Attribute>() {
-      @Override
-      public int compare(final Attribute o1, final Attribute o2) {
-         return Integer.compare(o1.offset, o2.offset);
-      }
-   };
-   
-   private final Attribute[] usedAttributes = new Attribute[USED_ATTRIBUTE_NAMES.length];
-   public final int stride;
-   
+   private static final int            BYTES_PER_POINT      = 32;
+   private static final String         X_ATTRIBUTE_NAME     = "X";
+   private static final String         Y_ATTRIBUTE_NAME     = "Y";
+   private static final String         Z_ATTRIBUTE_NAME     = "Z";
+   private static final String         R_ATTRIBUTE_NAME     = "Red";
+   private static final String         G_ATTRIBUTE_NAME     = "Green";
+   private static final String         B_ATTRIBUTE_NAME     = "Blue";
+   private static final String         A_ATTRIBUTE_NAME     = "Altitude";
+   private static final String         I_ATTRIBUTE_NAME     = "Intensity";
+
+   private static final String[]       USED_ATTRIBUTE_NAMES = new String[] { DataAttributes.X_ATTRIBUTE_NAME, DataAttributes.Y_ATTRIBUTE_NAME, DataAttributes.Z_ATTRIBUTE_NAME, DataAttributes.R_ATTRIBUTE_NAME,
+         DataAttributes.G_ATTRIBUTE_NAME, DataAttributes.B_ATTRIBUTE_NAME, DataAttributes.A_ATTRIBUTE_NAME, DataAttributes.I_ATTRIBUTE_NAME };
+
+   private static final boolean[]      normalize            = new boolean[] { false, false, false, true, true, true, false, true };
+
+   private final List<Attribute>       attributes           = new ArrayList<>();
+   private final Comparator<Attribute> indexComparator      = new Comparator<Attribute>() {
+                                                               @Override
+                                                               public int compare(final Attribute o1, final Attribute o2) {
+                                                                  return Integer.compare(o1.offset, o2.offset);
+                                                               }
+                                                            };
+
+   private final Attribute[]           usedAttributes       = new Attribute[DataAttributes.USED_ATTRIBUTE_NAMES.length];
+   public final int                    stride;
+
    public DataAttributes(final List<Attribute> attributes) {
       this.attributes.addAll(attributes);
-      this.attributes.sort(indexComparator);
-      
+      this.attributes.sort(this.indexComparator);
+
       final Attribute last = this.attributes.get(this.attributes.size() - 1);
       this.stride = last.offset + last.size;
-      
-      for(int i = 0; i < USED_ATTRIBUTE_NAMES.length; i++) {
+
+      for (int i = 0; i < DataAttributes.USED_ATTRIBUTE_NAMES.length; i++) {
          // sets to null if unavailable
-         usedAttributes[i] = this.getAttribute(USED_ATTRIBUTE_NAMES[i]);
+         this.usedAttributes[i] = this.getAttribute(DataAttributes.USED_ATTRIBUTE_NAMES[i]);
       }
    }
-   
-   public int getGpuSize() {
-      return BYTES_PER_POINT;
-   }
-   
-   public int size() {
-      return attributes.size();
-   }
-   
+
    public Attribute getAttribute(final String name) {
       Attribute foundAttribute = null;
-      for(final Attribute attribute : this.attributes) {
-         if(attribute.name.equals(name)) {
+      for (final Attribute attribute : this.attributes) {
+         if (attribute.name.equals(name)) {
             foundAttribute = attribute;
             break;
          }
       }
-         
+
       return foundAttribute;
    }
-   
+
    public String getAttributeNames() {
       final StringBuilder sb = new StringBuilder();
-      
-      for(int i = 0; i < attributes.size(); i++) {
-         if(i != 0) {
+
+      for (int i = 0; i < this.attributes.size(); i++) {
+         if (i != 0) {
             sb.append(",");
          }
-         sb.append(attributes.get(i).name);
+         sb.append(this.attributes.get(i).name);
       }
-      
+
       return sb.toString();
+   }
+
+   public int getGpuSize() {
+      return DataAttributes.BYTES_PER_POINT;
    }
 
    @Override
    public Iterator<Attribute> iterator() {
       return this.attributes.iterator();
    }
-   
+
    /**
     * Will load into buffer X,Y,Z,R,G,B,Altitude,Intensity as float values.
-    * 
+    *
     * @param buffer
     * @param pointIndex
     */
-   public void loadBuffer(final Tuple3d origin, final ByteBuffer buffer, final ByteBuffer pointData, final int pointIndex) {
-//      double x = 0, y = 0, z = 0;
-      
-      for(int i = 0; i < this.usedAttributes.length; i++) {
+   public void loadBuffer(final Tuple3d origin, final ByteBuffer buffer, final ByteBuffer pointData, final int pointIndex, final Tuple3d outXyz) {
+      for (int i = 0; i < this.usedAttributes.length; i++) {
          final Attribute attribute = this.usedAttributes[i];
          final boolean normalize = DataAttributes.normalize[i];
-         
-         if(attribute == null) {
+
+         if (attribute == null) {
             buffer.putFloat(0f);
          } else {
             double value = attribute.getValue(pointData, pointIndex, this.stride).doubleValue();
-            
-            if(i == 0) {
+
+            if (i == 0) {
+               if (outXyz != null) {
+                  outXyz.x = value;
+               }
                value -= origin.x;
-            } else if(i == 1) {
+            } else if (i == 1) {
+               if (outXyz != null) {
+                  outXyz.y = value;
+               }
                value -= origin.y;
-            } else if(i == 2) {
+            } else if (i == 2) {
+               if (outXyz != null) {
+                  outXyz.z = value;
+               }
                value -= origin.z;
             }
-            
-            if(normalize) {
+
+            if (normalize) {
                value /= Math.pow(2, attribute.size * 8); // 2^numBits
             }
 
             buffer.putFloat((float) value);
          }
       }
+   }
+
+   public int size() {
+      return this.attributes.size();
    }
 }
--- src/java/com/stephenwranger/thesis/selection/ContextAwarePointSelection.java
+++ src/java/com/stephenwranger/thesis/selection/ContextAwarePointSelection.java
@@ -17,12 +17,17 @@ import com.jogamp.opengl.fixedfunc.GLLightingFunc;
 import com.jogamp.opengl.fixedfunc.GLMatrixFunc;
 import com.jogamp.opengl.glu.GLU;
 import com.stephenwranger.graphics.Scene;
+import com.stephenwranger.graphics.color.Color4f;
 import com.stephenwranger.graphics.math.Tuple2d;
 import com.stephenwranger.graphics.math.Tuple3d;
 import com.stephenwranger.graphics.math.intersection.IntersectionUtils;
 import com.stephenwranger.graphics.math.intersection.LineSegment;
+import com.stephenwranger.graphics.math.intersection.Triangle3d;
 import com.stephenwranger.graphics.renderables.PointRenderable;
 import com.stephenwranger.graphics.renderables.PostProcessor;
+import com.stephenwranger.graphics.renderables.TriangleMesh;
+import com.stephenwranger.graphics.utils.TupleMath;
+import com.stephenwranger.thesis.data.GridCell;
 import com.stephenwranger.thesis.renderables.TreeRenderable;
 
 public class ContextAwarePointSelection implements PostProcessor, MouseListener, MouseMotionListener, MouseWheelListener {
@@ -30,9 +35,12 @@ public class ContextAwarePointSelection implements PostProcessor, MouseListener,
    private final TreeRenderable  tree;
    private final PointRenderable pointRenderer;
 
-   //   private final TriangleMesh    selectionTriangles = null;
-   private Volume                selection      = null;
-   private final List<Tuple2d>   mouseSelection = new ArrayList<>();
+   private TriangleMesh          selectionTriangles = null;
+   private Volume                selection          = null;
+   private final List<Tuple2d>   mouseSelection     = new ArrayList<>();
+
+   private double                minDensity         = 1.0;
+   private int                   gridSize           = 4;
 
    public ContextAwarePointSelection(final Scene scene, final TreeRenderable tree) {
       this.scene = scene;
@@ -45,6 +53,14 @@ public class ContextAwarePointSelection implements PostProcessor, MouseListener,
       this.pointRenderer = new PointRenderable();
       this.pointRenderer.setPointSize(10f);
       this.scene.addRenderable(this.pointRenderer);
+   }
+
+   public int getGridSizeMeters() {
+      return this.gridSize;
+   }
+
+   public double getMinDensity() {
+      return this.minDensity;
    }
 
    @Override
@@ -85,7 +101,11 @@ public class ContextAwarePointSelection implements PostProcessor, MouseListener,
          this.mouseSelection.add(new Tuple2d(event.getX(), event.getY()));
       } else {
          this.mouseSelection.clear();
-         this.pointRenderer.setPoints(null);
+
+         if (event.isControlDown()) {
+            this.pointRenderer.setPoints(null);
+         }
+
          this.selection = null;
       }
    }
@@ -122,7 +142,11 @@ public class ContextAwarePointSelection implements PostProcessor, MouseListener,
          this.selection = new Volume(this.scene, this.scene.getCameraPosition(), polygonVertices);
       } else {
          this.mouseSelection.clear();
-         this.pointRenderer.setPoints(null);
+
+         if (event.isControlDown()) {
+            this.pointRenderer.setPoints(null);
+         }
+
          this.selection = null;
       }
    }
@@ -139,18 +163,25 @@ public class ContextAwarePointSelection implements PostProcessor, MouseListener,
 
       if (selection != null) {
          final Collection<Tuple3d> selectedPoints = this.tree.getVolumeIntersection(selection);
+         final List<GridCell> cells = new ArrayList<>();
+         final double averageDensity = ContextAwarePointSelection.getAverageDensity(selection, selectedPoints, cells, this.gridSize);
          System.out.println("selected points count: " + selectedPoints.size());
+         System.out.println("average density: " + averageDensity);
          this.pointRenderer.setPoints(selectedPoints);
 
-         //         if (this.selectionTriangles != null) {
-         //            this.selectionTriangles.remove();
-         //            this.selectionTriangles = null;
-         //         }
-         //
-         //         this.selectionTriangles = new TriangleMesh(selection.getTriangles(), Color4f.red());
-         //         this.selectionTriangles.setPolygonMode(GL.GL_FRONT_AND_BACK);
-         //         this.selectionTriangles.setDrawNormals(true);
-         //         this.scene.addRenderable(this.selectionTriangles);
+         if (this.selectionTriangles != null) {
+            this.selectionTriangles.remove();
+            this.selectionTriangles = null;
+         }
+
+         final Triangle3d[] triangles = ContextAwarePointSelection.getTriangles(cells);
+
+         if (triangles.length > 0) {
+            this.selectionTriangles = new TriangleMesh(triangles, Color4f.red());
+            this.selectionTriangles.setPolygonMode(GL.GL_FRONT_AND_BACK);
+            this.selectionTriangles.setDrawNormals(false);
+            this.scene.addRenderable(this.selectionTriangles);
+         }
       } else if (!this.mouseSelection.isEmpty()) {
          final int size = this.mouseSelection.size();
          gl.glPushMatrix();
@@ -178,6 +209,82 @@ public class ContextAwarePointSelection implements PostProcessor, MouseListener,
       }
    }
 
+   public void setGridSizeMeters(final int gridSize) {
+      this.gridSize = gridSize;
+   }
+
+   public void setMinDensity(final double minDensity) {
+      this.minDensity = minDensity;
+   }
+
+   // TODO: fix grid size
+   private static double getAverageDensity(final Volume volume, final Collection<Tuple3d> points, final Collection<GridCell> outputCells, final int gridSize) {
+      if (points.isEmpty()) {
+         return 0;
+      }
+
+      // average density is computed as:
+      // pf = 1/nf * sigma(density(node[n]))
+
+      // first, split collected points into grid
+      final Tuple3d min = Tuple3d.getMin(points);
+      final Tuple3d max = Tuple3d.getMax(points);
+      final Tuple3d range = TupleMath.sub(max, min);
+      final int xSize = (int) Math.ceil(range.x / gridSize);
+      final int ySize = (int) Math.ceil(range.y / gridSize);
+      final int zSize = (int) Math.ceil(range.z / gridSize);
+
+      // then, determine density of points per grid node and sum them all
+      // for us, points per grid node is the density so we just need to determine
+      // how many points we have and then divide it by the number of nodes within the lasso
+      final double density = points.size();
+
+      // TODO: don't count the grid cubes outside the lasso
+      // TODO: for now, just points per cubic meter = point count / (xSize * ySize * zSize)
+      // average all the nodes' densities by dividing the sum by the number of nodes
+      final GridCell[][][] cells = new GridCell[xSize][ySize][zSize];
+      final double halfGrid = gridSize / 2.0;
+      int nodeCount = 0;
+
+      for (int x = 0; x < xSize; x++) {
+         for (int y = 0; y < ySize; y++) {
+            for (int z = 0; z < zSize; z++) {
+               final double px = min.x + (x * gridSize);
+               final double py = min.y + (y * gridSize);
+               final double pz = min.z + (z * gridSize);
+
+               if (volume.contains(new Tuple3d(px + halfGrid, py + halfGrid, pz + halfGrid), halfGrid)) {
+                  nodeCount++;
+                  cells[x][y][z] = new GridCell(new Tuple3d(px, py, pz), new Tuple3d(px + gridSize, py + gridSize, pz + gridSize));
+               }
+            }
+         }
+      }
+
+      for (final Tuple3d point : points) {
+         final int x = (int) Math.floor((point.x - min.x) / gridSize);
+         final int y = (int) Math.floor((point.y - min.y) / gridSize);
+         final int z = (int) Math.floor((point.z - min.z) / gridSize);
+
+         if (cells[x][y][z] != null) {
+            cells[x][y][z].points.add(point);
+         }
+      }
+
+      for (int x = 0; x < xSize; x++) {
+         for (int y = 0; y < ySize; y++) {
+            for (int z = 0; z < zSize; z++) {
+               if (cells[x][y][z] != null) {
+                  cells[x][y][z].finish();
+                  outputCells.add(cells[x][y][z]);
+               }
+            }
+         }
+      }
+
+      return density / nodeCount;
+   }
+
    private static Tuple2d getIntersections(final LineSegment toCheck, final List<LineSegment> existing) {
       final Tuple2d intersection = new Tuple2d();
 
@@ -191,5 +298,15 @@ public class ContextAwarePointSelection implements PostProcessor, MouseListener,
       }
 
       return null;
+   }
+
+   private static Triangle3d[] getTriangles(final List<GridCell> cells) {
+      final List<Triangle3d> triangles = new ArrayList<>();
+
+      for (final GridCell cell : cells) {
+         triangles.addAll(cell.triangles);
+      }
+
+      return triangles.toArray(new Triangle3d[triangles.size()]);
    }
 }
